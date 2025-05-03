545bd64bd6787ad5c18f44f6dfbf6e7cef0c82a5
Kutlwano Mokubetsi <2673228@students.wits.ac.za>
2025-05-03 18:49:38 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // src/App.js
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import AdminPage from "./pages/AdminPage";
 import SuperAdminPage from "./pages/SuperAdminPage"; // Add this import
