ad7c990ced64e627ceffd4a6ce958ce13c6ea41f
Kutlwano Mokubetsi <2673228@students.wits.ac.za>
2025-05-05 01:33:37 +0200

    New Test

--- src/__tests__/AdminPage.test.js
+++ src/__tests__/AdminPage.test.js
@@ -1,4 +1,5 @@
 // AdminPage.test.js
+import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import { useNavigate } from 'react-router-dom';
 import AdminPage from '../pages/AdminPage';
